a5d016c0b79f628ae8caf78acc6a289de046a461
Raghu <raghunathsingh2410@gmail.com>
2025-08-08 19:53:10 +0530

    added yaml for Deployement

--- portfolio_admin_back/portfolio_admin_back/settings.py
+++ portfolio_admin_back/portfolio_admin_back/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import os
+
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dummy-insecure-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -45,8 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
